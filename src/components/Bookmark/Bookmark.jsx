899422fa8b9d316298f75ef4f124f89eafd5996a
newbie-saimur <saimurrahmanarnob@gmail.com>
2025-09-21 00:36:11 +0600

    Minor fix

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -3,7 +3,7 @@ import { RxCross2 } from "react-icons/rx";
 const Bookmark = ({ bookmark, handleRemoveFromBookmark }) => {
     return (
         <div className="p-5 bg-white rounded-xl relative">
-            <button className="absolute top-3 right-1 -translate-y-2 hover:opacity-60 cursor-pointer" onClick={() => handleRemoveFromBookmark(bookmark)}><RxCross2 /></button>
+            <button className="absolute top-3 right-1 -translate-y-2 opacity-20 hover:opacity-100 cursor-pointer" onClick={() => handleRemoveFromBookmark(bookmark)}><RxCross2 /></button>
             <p className="text-[#111] text-[18px] font-semibold leading-7">{bookmark}</p>
         </div>
     );
